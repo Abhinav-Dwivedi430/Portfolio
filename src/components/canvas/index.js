2f129a78544e1dfa3d4a45cfea81827c9fbe3b1f
Abhinav-Dwivedi430 <abhinavdwivedi530@gmail.com>
2025-02-22 18:27:24 +0530

    Emptied beta branch

--- src/components/canvas/index.js
+++
@@ -1,7 +0,0 @@
-import EarthCanvas from "./Earth";
-import CircularRing from "./Skills";
-import SkullModel from "./Model";
-import StarsCanvas from "./Stars";
-
-
-export { StarsCanvas, EarthCanvas, CircularRing, SkullModel };
