26576bb23e5414f0826c7da69dad00edd0454460
Abhinav Dwivedi <72121682+Abhinav-Dwivedi430@users.noreply.github.com>
2025-11-09 22:11:27 +0530

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "../index.css"; // Ensure the CSS file exists for styling.
 
 const DownloadButton = () => {
-    const resumeUrl = "https://github.com/Abhinav-Dwivedi430/Resume/edit/main/Abhinav_Dwivedi.md`";
+    const resumeUrl = "https://github.com/Abhinav-Dwivedi430/Resume/edit/main/Abhinav_Dwivedi.md";
 
     return (
         <div className="button-container">
